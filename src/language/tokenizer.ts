9883786de94b834973b9c0eb1f107e4fc037a19d
GHSoares <ghsoares99795@gmail.com>
2021-11-29 11:54:31 -0300

    Added some tokenizer documentation

--- src/language/tokenizer.ts
+++ src/language/tokenizer.ts
@@ -7,14 +7,44 @@ export function throwAtPos(line: number, column: number, msg: string) {
 	return e;
 }
 
+/**
+ * Main tokenizer class, takes multiple token definitions with it's type and regex and tokenizes
+ * a source string into tokens.
+ */
 export default class Tokenizer {
+	/**
+	 * Skips tokens to be ignored, turn this off if want to use these ignored tokens for
+	 * syntax highlighting, for example
+	 */
 	public skipIgnore: boolean;
+	/**
+	 * Array containing all token definitions, identified by it's type and contains regex expression and
+	 * ignore flag.
+	 */
 	private tokenDefs: TokenDefinition[];
+	/**
+	 * The current source being tokenized
+	 */
 	private source: string;
+	/**
+	 * The length of the current source being tokenized
+	 */
 	private sourceLength: number;
+	/**
+	 * The source string cursor, points at a character on the position of this cursor
+	 */
 	private cursor: number;
+	/**
+	 * All the tokens from the tokenization step
+	 */
 	private tokens: Token[];
+	/**
+	 * Total number of tokens from the tokenization step
+	 */
 	private numTokens: number;
+	/**
+	 * Cursor that points at a token
+	 */
 	private tokenCursor: number;
 
 	public constructor() {
@@ -25,11 +55,21 @@ export default class Tokenizer {
 		this.cursor = 0;
 	}
 
+	/**
+	 * Adds a token definition
+	 * @param {any} type - The type of the token, can be any type of value
+	 * @param {RegExp} regex - The regex expression used to tokenize this token
+	 * @param {boolean} [ignore=false] - Should this token be ignored? Use this for whitespaces or comments
+	 */
 	public addTokenDefinition(type: any, regex: RegExp, ignore: boolean = false): void {
 		regex = new RegExp(`^${regex.source}`);
 		this.tokenDefs.push({ type, regex, ignore });
 	}
 
+	/**
+	 * Initializes the tokenization step
+	 * @param {string} source - The actual source code used to tokenize
+	 */
 	private init(source: string): void {
 		this.source = source;
 		this.sourceLength = source.length;
@@ -39,10 +79,18 @@ export default class Tokenizer {
 		this.tokenCursor = 0;
 	}
 
+	/**
+	 * Is the tokenization step reached EOF?
+	 * @returns {boolean} Reached EOF?
+	 */
 	private EOF(): boolean {
 		return this.cursor >= this.sourceLength;
 	}
 
+	/**
+	 * Gets the next token from the source string cursor
+	 * @returns {Token} The next token
+	 */
 	private getNextToken(): Token {
 		const str = this.source.slice(this.cursor);
 		let tk: Token = null;
@@ -111,6 +159,11 @@ export default class Tokenizer {
 		return tk;
 	}
 
+	/**
+	 * Gets additional cursor information at position
+	 * @param {number} pos - The cursor position
+	 * @returns {[number, number]} Line and column of the cursor
+	 */
 	public getCursorInfo(pos: number): [number, number] {
 		let [cursorLine, cursorColumn] = [0, -1];
 		for (let i = 0; i <= pos; i++) {
@@ -125,6 +178,10 @@ export default class Tokenizer {
 		return [cursorLine, cursorColumn];
 	}
 
+	/**
+	 * Main tokenization function, tokenizes the entire source string
+	 * @param {string} str - The source string
+	 */
 	public tokenize(str: string): void {
 		this.init(str);
 		while (true) {
@@ -135,8 +192,18 @@ export default class Tokenizer {
 		}
 	}
 
+	/**
+	 * Returns the current token cursor position
+	 * @returns {number} position
+	 */
 	public tkCursor(): number { return this.tokenCursor; }
 
+	/**
+	 * Returns the current token that matches the type or null if it don't match.
+	 * If it matches, it advances to the next token
+	 * @param {any} type - The token type to match 
+	 * @returns {Token} The current token or null
+	 */
 	public tk(type: any): Token {
 		if (this.tokenCursor >= this.numTokens) return null;
 
@@ -149,6 +216,10 @@ export default class Tokenizer {
 		return null;
 	}
 
+	/**
+	 * Returns the current token
+	 * @returns {Token} The current token
+	 */
 	public currTk(): Token {
 		if (this.tokenCursor >= this.numTokens) return {
 			type: "EOF",
@@ -161,14 +232,34 @@ export default class Tokenizer {
 		return this.tokens[this.tokenCursor];
 	}
 
+	/**
+	 * Go back one pos of the token cursor
+	 */
+	/**
+	 * Returns the token position to the position provided
+	 * @param {number} pos - The position to return 
+	 * @returns {null}
+	 */
 	public tkRet(pos: number): null { this.tokenCursor = pos; return null; }
 
+	/**
+	 * Throws the error message at the current token position
+	 * @param {string} msg - The error message
+	 */
 	public tkThrowErr(msg: string) {
 		const currTk = this.currTk();
 		throw new SyntaxError(`At line ${currTk.line+1} column ${currTk.column+1}: ${msg}`);
 	}
 
+	/**
+	 * Gets the number of tokens left to use
+	 * @returns {number} The number of left tokens
+	 */
 	public tokensLeft(): number { return this.numTokens - this.tokenCursor; }
 
+	/**
+	 * Returns a copy of the tokenized tokens
+	 * @returns {Token[]} The array of tokens copy
+	 */
 	public getTokens(): Token[] { return [...this.tokens]; }
 }
