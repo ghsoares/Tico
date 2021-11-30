52af51d65a73f1a04ba4ad6c6b6a53425a94b071
GHSoares <ghsoares99795@gmail.com>
2021-11-30 12:25:10 -0300

    Updated package

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,13 @@
 import TicoParser from "./language/ticoParser";
+import TicoTokenizer from "./language/ticoTokenizer";
 import TicoProgram from "./runtime/tico";
-import fs from "fs";
 
-const source = fs.readFileSync('src/test.tico', 'utf-8');
+export function createWorker() {
+	return new Worker('./webWorker/index.ts');
+}
 
-const start = Date.now();
-
-const parser = new TicoParser();
-const main = parser.parse(source);
-const program = new TicoProgram(main);
-const str = TicoParser.stringify(
-	main,
-	{ showPosition: false }
-);
-const astLines = str.split("\n").length - 1;
-
-process.stdout.write(str + "\n");
-process.stdout.write(`AST has ${astLines} lines\n`);
-
-class Foo {
-	public value: number;
-
-	constructor(value: number) {
-		this.value = value;
-	}
-
-	static add(a: any, b: any): Foo {
-		if (a instanceof Foo && b instanceof Foo) {
-			return new Foo(a.value + b.value);
-		}
-		if (a instanceof Foo && typeof b === 'number') {
-			return new Foo(a.value + b);
-		}
-
-		return null;
-	}
+export {
+	TicoParser,
+	TicoTokenizer,
+	TicoProgram
 };
-
-console.log(program.run({}, {
-	Foo: (value: number) => new Foo(value)
-}));
-
-const elapsed = Date.now() - start;
-
-console.log(`Elapsed: ${elapsed} ms`);
